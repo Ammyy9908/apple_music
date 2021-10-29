a205098d0c531aff60dfad6bd7757fa73a658bee
ammyy9908 <sb78639@gmail.com>
2021-10-29 10:57:35 +0530

    state mgmt added with song play,next,prev added.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import Body from '../components/Body'
 import Sidebar from '../components/Sidebar'
 import "./Main.css"
-
+import Cookies from "js-cookie";
+import {useLocation} from 'react-router-dom'
 function Main() {
+    function useQuery() {
+        return new URLSearchParams(useLocation().search);
+      }
+      let query = useQuery();
+     
+      const token=  query.get('access_token')
+      if(token){
+        Cookies.set('SPOTIFY_TOKEN',token);
+        window.location.href='/'
+      }
+      
+      
     return (
         <div className="main-screen">
             <Sidebar/>
